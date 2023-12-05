6e6e64e75fe498113dea6c15c2e57d70154435c5
Joel Ekebro <joelekebro@MacBook-Air-som-tillhor-Joel.local>
2023-12-05 11:25:45 +0100

    more tests

--- test.m
+++ test.m
@@ -1,7 +1,8 @@
 epsilon = 0.1;
-tol = 1e-4;
+tol = 1e-3;
 restart = 0;
-printout = 1;
+printout = 0;
+fprintf('\n%12s %12s %12s %12s %12s %15s\n\n','Method','N', 'iter', 'f(x)', 'norm(grad)', 'ls fun evals');
 
 %% Test 1
 
@@ -13,4 +14,112 @@ F = @(x) x' * A * x;
 x0 = 5 *rand(10, 1);
 
 [x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 2
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+%% Test 3
+
+N = 20;
+A = 10 * rand(N, N);
+A = A*A' + epsilon*eye(N);
+
+F = @(x) x' * A * x;
+x0 = 5 *rand(N, 1);
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 4
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+%% Test 5
+
+N = 30;
+A = 10 * rand(N, N);
+A = A*A' + epsilon*eye(N);
+
+F = @(x) x' * A * x;
+x0 = 5 *rand(N, 1);
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 6
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+%% Test 7
+
+N = 40;
+A = 10 * rand(N, N);
+A = A*A' + epsilon*eye(N);
+
+F = @(x) x' * A * x;
+x0 = 5 *rand(N, 1);
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 8
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+%% Test 9
+
+N = 100;
+A = 10 * rand(N, N);
+A = A*A' + epsilon*eye(N);
+
+F = @(x) x' * A * x;
+x0 = 5 *rand(N, 1);
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 10
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+%% Test 11
+
+N = 2;
+A = 10 * rand(N, N);
+A = A*A' + epsilon*eye(N);
+
+F = @(x) x' * A * x;
+x0 = 5 *rand(N, 1);
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'DFP', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','DFP', N, N_iter, F(x), normg, N_eval)
+
+%% Test 12
+
+[x, N_eval, N_iter, normg] = nonlinearmin(F, x0, 'BFGS', tol, restart, printout);
+fprintf('%12s %12.4f %12.4f %12.4f %12f %12.4f\n','BFGS', N, N_iter, F(x), normg, N_eval)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
